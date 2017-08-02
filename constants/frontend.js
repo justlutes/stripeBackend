021f1e01766d7104c825aa55bca5ad9dd19548cb
klutes90 <klutes90@gmail.com>
2017-08-02 16:09:00 -0400

    updating urls

--- constants/frontend.js
+++ constants/frontend.js
@@ -1,7 +1,11 @@
-const FRONTEND_DEV_URLS = ['http://localhost:3000'];
+const FRONTEND_DEV_URLS = ["http://localhost:3000"];
 
-const FRONTEND_PROD_URLS = ['https://www.kcwedding.us', 'https://kcwedding.us'];
+const FRONTEND_PROD_URLS = [
+  "https://desolate-inlet-17426.herokuapp.com",
+  "https://www.kcwedding.us"
+];
 
-module.exports = process.env.NODE_ENV === 'production'
-  ? FRONTEND_PROD_URLS
-  : FRONTEND_DEV_URLS;
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? FRONTEND_PROD_URLS
+    : FRONTEND_DEV_URLS;
